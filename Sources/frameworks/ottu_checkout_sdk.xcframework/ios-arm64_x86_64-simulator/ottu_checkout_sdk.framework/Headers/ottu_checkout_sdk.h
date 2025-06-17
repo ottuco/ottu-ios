415d4eb9d0a2c97ffa99ed8ac0128da11b29e898
Admin <admin@Admins-Mac-mini.local>
2025-06-17 14:26:12 +0300

    Release 2.1.0

---
+++ Sources/frameworks/ottu_checkout_sdk.xcframework/ios-arm64_x86_64-simulator/ottu_checkout_sdk.framework/Headers/ottu_checkout_sdk.h
@@ -0,0 +1,18 @@
+//
+//  ottu_ios_sdk.h
+//  ottu-ios-sdk
+//
+//  Created by Ottu on 08.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ottu_ios_sdk.
+FOUNDATION_EXPORT double ottu_ios_sdkVersionNumber;
+
+//! Project version string for ottu_ios_sdk.
+FOUNDATION_EXPORT const unsigned char ottu_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ottu_ios_sdk/PublicHeader.h>
+
+
