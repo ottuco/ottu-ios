6578f426e36ff1af6bdb7c5450aef89e947f698a
Admin <admin@Admins-Mac-mini.local>
2026-06-22 14:26:28 +0300

    Test release with Sentry

--- Package.swift
+++ Package.swift
@@ -2,20 +2,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "ottu_checkout_sdk",
+    name: "ottu_checkout_sdk_sentry",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "ottu_checkout_sdk",
-            targets: ["ottu_checkout_sdk"]
+            name: "ottu_checkout_sdk_sentry",
+            targets: ["ottu_checkout_sdk_sentry", "ottu_checkout_sdk_dep"]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "9.0.0")
     ],
     targets: [
         .binaryTarget(
-            name: "ottu_checkout_sdk",
-            path: "./Sources/ottu_checkout_sdk.xcframework"
+            name: "ottu_checkout_sdk_sentry",
+            path: "./Sources/ottu_checkout_sdk_sentry.xcframework"
+        ),
+        .target(
+            name: "ottu_checkout_sdk_dep",
+            dependencies: [
+                         .product(name: "Sentry", package: "sentry-cocoa")
+                     ]
         )
     ]
 )
