05919064b5b10fe1a5d2d2936ff1316a59f0960e
Admin <admin@Admins-Mac-mini.local>
2026-06-24 14:40:12 +0300

    Adjusted Swift package

--- Package.swift
+++ Package.swift
@@ -7,28 +7,26 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-    	.library(
+        .library(
             name: "ottu_checkout_sdk_sentry",
-            targets: ["ottu_checkout_sdk_sentry_wrapper"]
+            targets: ["ottu_checkout_sdk_sentry_dep"]
         )
     ],
-
     dependencies: [
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "9.0.0")
     ],
-
     targets: [
         .binaryTarget(
             name: "ottu_checkout_sdk_sentry",
-            path: "./Sources/ottu_checkout_sdk_sentry.xcframework"
+            path: "Sources/ottu_checkout_sdk_sentry.xcframework"
         ),
-
         .target(
-            name: "ottu_checkout_sdk_sentry_wrapper",
+            name: "ottu_checkout_sdk_sentry_dep",
             dependencies: [
                 "ottu_checkout_sdk_sentry",
                 .product(name: "Sentry", package: "sentry-cocoa")
-            ]
+            ],
+            path: "Sources/ottu_checkout_sdk_sentry_dep"
         )
     ]
 )
