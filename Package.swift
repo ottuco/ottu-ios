dcc1777aa824969b0d5ea76ebaf57420606996b1
Admin <admin@Admins-Mac-mini.local>
2026-06-29 13:58:08 +0300

    Test release without dependency

--- Package.swift
+++ Package.swift
@@ -9,22 +9,13 @@ let package = Package(
     products: [
         .library(
             name: "ottu_checkout_sdk",
-            targets: ["ottu_checkout_sdk", "ottu_checkout_sdk_dep"]
+            targets: ["ottu_checkout_sdk"]
         )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.46.0")
     ],
     targets: [
         .binaryTarget(
             name: "ottu_checkout_sdk",
             path: "./Sources/ottu_checkout_sdk.xcframework"
-        ),
-        .target(
-            name: "ottu_checkout_sdk_dep",
-            dependencies: [
-                         .product(name: "Sentry", package: "sentry-cocoa")
-                     ]
         )
     ]
 )
