59973bcfe4d04a359361790cfd3b987ff437cf01
Admin <admin@Admins-Mac-mini.local>
2026-06-30 16:17:58 +0300

    Frameworks as zip

--- Package.swift
+++ Package.swift
@@ -19,12 +19,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ottu_checkout_sdk",
-            path: "Sources/ottu_checkout_sdk.xcframework"
+            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-6/ottu_checkout_sdk.xcframework.zip",
+            checksum: "ed5559912955dedcef42f2113a0a7fcba8777c498d90b0f22f0153ac60d8b0a6"
         ),
         .binaryTarget(
             name: "ottu_checkout_sdk_sentry",
-            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-5/ottu_checkout_sdk_sentry.xcframework.zip",
-            checksum: "ed5559912955dedcef42f2113a0a7fcba8777c498d90b0f22f0153ac60d8b0a6"
+            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-6/ottu_checkout_sdk_sentry.xcframework.zip",
+            checksum: "5d96849ad1b4d3b4c5411d9bbdbaa56c86251581f986ffe855df7a159a83ca99"
         )
     ]
 )
