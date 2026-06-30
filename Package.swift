14abd833abf40f5a3bdff8e4c7aa97da4ed49887
Admin <admin@Admins-Mac-mini.local>
2026-06-30 16:22:22 +0300

    Corrected checksums

--- Package.swift
+++ Package.swift
@@ -20,12 +20,12 @@ let package = Package(
         .binaryTarget(
             name: "ottu_checkout_sdk",
             url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-6/ottu_checkout_sdk.xcframework.zip",
-            checksum: "ed5559912955dedcef42f2113a0a7fcba8777c498d90b0f22f0153ac60d8b0a6"
+            checksum: "5d96849ad1b4d3b4c5411d9bbdbaa56c86251581f986ffe855df7a159a83ca99"
         ),
         .binaryTarget(
             name: "ottu_checkout_sdk_sentry",
             url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-6/ottu_checkout_sdk_sentry.xcframework.zip",
-            checksum: "5d96849ad1b4d3b4c5411d9bbdbaa56c86251581f986ffe855df7a159a83ca99"
+            checksum: "ed5559912955dedcef42f2113a0a7fcba8777c498d90b0f22f0153ac60d8b0a6"
         )
     ]
 )
