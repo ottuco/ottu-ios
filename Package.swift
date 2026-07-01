05377b3f5209d9f519e7625db8059da5a12517a3
Admin <admin@Admins-Mac-mini.local>
2026-07-01 13:35:40 +0300

    Corrected links

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ottu_checkout_sdk",
-            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-6/ottu_checkout_sdk.xcframework.zip",
-            checksum: "5d96849ad1b4d3b4c5411d9bbdbaa56c86251581f986ffe855df7a159a83ca99"
+            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-8/ottu_checkout_sdk.xcframework.zip",
+            checksum: "c8b4e81419188c713309aab57ff4c01eb7649ac3aeac05e387a610f54ee255f8"
         ),
         .binaryTarget(
             name: "ottu_checkout_sdk_sentry",
-            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-6/ottu_checkout_sdk_sentry.xcframework.zip",
-            checksum: "ed5559912955dedcef42f2113a0a7fcba8777c498d90b0f22f0153ac60d8b0a6"
+            url: "https://github.com/ottuco/ottu-ios/releases/download/2.2.12-no-deps-8/ottu_checkout_sdk_sentry.xcframework.zip",
+            checksum: "344f54fc0b3abeff42a5f5044164cdddd493272265999e1b57d60b8e426e7a8f"
         )
     ]
 )
